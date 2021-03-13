dc1bd8d8406febf37eaa1c73ed261622d210c955
Leonardo Moreno Urbieta <leourbieta@MacBook-Pro-de-Leonardo.local>
2021-03-13 12:02:29 -0600

    Modificacion de tests

--- test/controllers/peticiones_controller_test.rb
+++ test/controllers/peticiones_controller_test.rb
@@ -4,6 +4,9 @@ class PeticionesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @usuario = User.create!(nombre_usuario: "Leo", password: "password")
+    @peticion = Peticion.create!(nombre_trabajador: "Gerardo", apellido_paterno: "Gonzalez",
+    apellido_materno: "Martinez", fecha_para_realizar_tramite: Date.parse("01-01-2001"), fecha_nacimiento: Date.parse("04-04-1984"),
+    empresa_solicitante: "NEO S.A. DE C.V.", persona_solicitante: "Juan Sanchez", movimiento: "Baja")
   end
 
   test "should get index" do
