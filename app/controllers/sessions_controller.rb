3b5af63bd9d7f2bfea3f6999e5b4e93501e2fb05
Leonardo Moreno Urbieta <leourbieta@MacBook-Pro-de-Leonardo.local>
2021-03-16 13:25:24 -0600

    Agregar reset_sessions a sessions controller create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     usuario = User.find_by(nombre_usuario: params[:session][:nombre_usuario])
     if usuario && usuario.authenticate(params[:session][:password])
+      reset_session
       session[:usuario_id] = usuario.id
       flash[:success] = "Has entrado con éxito"
       redirect_to peticiones_path
@@ -17,6 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    reset_session
     session[:usuario_id] = nil
     flash[:success] = "Has salido de tu sesión"
     redirect_to root_path
