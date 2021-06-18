625607a4cf16e0b22164ebec0d4d86bb952397af
Leo <mfiscalistas@gmail.com>
2021-06-18 17:47:21 -0500

    Mejoras en view de index clientes

--- app/javascript/packs/funcional.js
+++ app/javascript/packs/funcional.js
@@ -2,8 +2,6 @@ window.copyToClipboard = function(event) {
 
   var str = event.target.textContent;
   str = str.replace(/\s+/g,"");
-  //var dos_puntos = str.indexOf(':');
-  //var contenido = str.substring(dos_puntos+1); 
   function listener(e) {
     e.clipboardData.setData("text/html", str);
     e.clipboardData.setData("text/plain",str);
