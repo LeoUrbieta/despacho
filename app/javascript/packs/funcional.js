7b9a2cc10b61fe4c279d88f5a4f3d924fa6fa845
Leo <mfiscalistas@gmail.com>
2021-06-18 14:37:41 -0500

    Cambiar view de index clientes para mayor facilidad

--- app/javascript/packs/funcional.js
+++ app/javascript/packs/funcional.js
@@ -2,11 +2,11 @@ window.copyToClipboard = function(event) {
 
   var str = event.target.textContent;
   str = str.replace(/\s+/g,"");
-  var dos_puntos = str.indexOf(':');
-  var contenido = str.substring(dos_puntos+1); 
+  //var dos_puntos = str.indexOf(':');
+  //var contenido = str.substring(dos_puntos+1); 
   function listener(e) {
-    e.clipboardData.setData("text/html", contenido);
-    e.clipboardData.setData("text/plain", contenido);
+    e.clipboardData.setData("text/html", str);
+    e.clipboardData.setData("text/plain",str);
     e.preventDefault();
   }
   document.addEventListener("copy", listener);
