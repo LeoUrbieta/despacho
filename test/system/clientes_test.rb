649e0214a3cb080a8e307e944b022fa1925cff68
Leo Moreno <leomoreno@hey.com>
2022-05-17 11:52:51 -0500

    Arreglar boton de Dar de baja cliente y añadir system tests de Clientes que incluyen esta función

--- test/system/clientes_test.rb
+++ test/system/clientes_test.rb
@@ -3,39 +3,51 @@ require "application_system_test_case"
 class ClientesTest < ApplicationSystemTestCase
   setup do
     @cliente = clientes(:one)
+    User.create!(nombre_usuario: 'leomoreno', password: 'leomoreno', admin: false)
+    visit root_path
+    fill_in with: 'leomoreno', id: 'session_nombre_usuario'
+    fill_in with: 'leomoreno', :id => 'session_password'
+    click_on "Entrar"
+    assert_text "Has entrado con éxito"
   end
 
   test "visiting the index" do
-    visit clientes_url
-    assert_selector "h1", text: "Clientes"
+    click_link_or_button 'Clientes'
+    assert_text "Clientes"
   end
 
   test "creating a Cliente" do
     visit clientes_url
-    click_on "New Cliente"
+    click_on "Nuevo Cliente"
+    fill_in with: "199", id: 'cliente_num_interno'
+    fill_in with: "EMPRESA SA DE CV", id: 'cliente_razon_social'
+    fill_in with: "EMP0000001N1", id: 'cliente_rfc'
+    click_on "Crear Cliente"
+    assert_text "El Cliente fue creado exitosamente"
+  end
 
-    click_on "Create Cliente"
-
-    assert_text "Cliente was successfully created"
-    click_on "Back"
+  test "dar de baja Cliente" do
+    visit clientes_url
+    click_link @cliente.razon_social
+    assert_text @cliente.num_interno
+    click_link_or_button "Dar de baja como Cliente"
+    assert_text "BAJA"
+    assert_text "El Cliente se actualizó exitosamente"
   end
 
   test "updating a Cliente" do
     visit clientes_url
-    click_on "Edit", match: :first
-
-    click_on "Update Cliente"
-
-    assert_text "Cliente was successfully updated"
-    click_on "Back"
+    click_link @cliente.razon_social
+    click_on "Editar Cliente"
+    fill_in with: "105", id: 'cliente_num_interno'
+    click_button "Actualizar Cliente"
+    assert_text "El Cliente se actualizó exitosamente"
   end
 
-  test "destroying a Cliente" do
+  test "usuario normal no tiene boton eliminar" do
     visit clientes_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Cliente was successfully destroyed"
+    click_link @cliente.razon_social
+    assert_text @cliente.num_interno
+    assert_no_button "Eliminar"
   end
 end
