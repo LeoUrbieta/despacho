21496050a29711c6e5a7f0eeba907163d5242531
Leo Moreno <leomoreno@hey.com>
2022-07-27 13:26:49 -0500

    Actualizar lista de obligaciones y mejorar informacion disponible en view de contabilidad

--- app/models/obligacion.rb
+++ app/models/obligacion.rb
@@ -7,6 +7,10 @@ class Obligacion < ApplicationRecord
      {'IVA' => "IVA mes"},
      {'ISR' => 'ISR mes'},
      {'DIOT' => 'DIOT mes'},
+     {'ISR Retenciones Sueldos y Salarios' => 'ISR Retenciones Sueldos y Salarios'},
+     {'ISR Arrendamiento' => 'ISR Arrendamiento'},
+     {'IVA Retenciones' => 'IVA Retenciones'},
+     {'ISR Retenciones Asimilados a Salarios' => 'ISR Retenciones Asimilados a Salarios'}
     ]
   end
 
