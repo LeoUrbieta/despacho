5a83b8f019aeaaadca552488104e6583c4947fff
Leo Moreno <leomoreno@hey.com>
2022-08-01 10:51:06 -0500

    Test para verificar que fecha única deba ser solo por cliente

--- test/models/obligacion_test.rb
+++ test/models/obligacion_test.rb
@@ -3,17 +3,23 @@ require "test_helper"
 class ObligacionTest < ActiveSupport::TestCase
 
   def setup
-    @cliente = clientes(:one)
-    obligacion_con_fecha = Obligacion.create!(cliente_id: @cliente.id,
+    @cliente_uno = clientes(:one)
+    @cliente_dos = clientes(:two)
+    obligacion_con_fecha = Obligacion.create!(cliente_id: @cliente_uno.id,
                                               fecha: "2022-07-01",
                                               presentadas: ["","Obligacion1","Obligacion2"])
   end
 
-  test "fecha debe ser unica" do
+  test "fecha debe ser unica por cliente" do
     assert_no_difference 'Obligacion.count', 'La fecha debe de ser unica' do
-    obligacion_con_fecha = Obligacion.create(cliente_id: @cliente.id,
-                                              fecha: "2022-07-01",
-                                              presentadas: ["","Obligacion1","Obligacion2"])
+      Obligacion.create(cliente_id: @cliente_uno.id,
+                        fecha: "2022-07-01",
+                        presentadas: ["","Obligacion1","Obligacion2"])
     end
+    assert_difference 'Obligacion.count' do
+      Obligacion.create!(cliente_id: @cliente_dos.id,
+                        fecha: "2022-07-01",
+                        presentadas: ["","ObligacionA","ObligacionB"]) 
+    end 
   end
 end
