961b22633e41f68c2bccc82e120756687e50de31
Leo Moreno <leomoreno@hey.com>
2022-08-01 10:58:37 -0500

    Solo hacer mas compacta la validacion de uniqueness en fecha por cada cliente

--- app/models/obligacion.rb
+++ app/models/obligacion.rb
@@ -2,8 +2,7 @@ class Obligacion < ApplicationRecord
 
   belongs_to :cliente
 
-  validates :fecha, presence: true
-  validates_uniqueness_of :fecha, scope: :cliente_id
+  validates :fecha, presence: true, uniqueness: {scope: :cliente_id}
 
   default_scope -> { order(fecha: :desc)}
 
