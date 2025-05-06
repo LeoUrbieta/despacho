08b5da340ece9bfb2e1d9a47af430124be890445
Leo Moreno <leomoreno@hey.com>
2025-05-06 16:21:11 -0600

    Add config.assets.compile to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,4 +108,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  #
+  # Agregado a sugerencia de Heroku
+  config.assets.compile = false
 end
