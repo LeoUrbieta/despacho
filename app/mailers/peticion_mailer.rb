cbe6cdff175edec1f6727c4051a7d56923522c14
Leo <leomoreno@hey.com>
2025-07-20 18:13:35 -0600

    Update mail parameters

--- app/mailers/peticion_mailer.rb
+++ app/mailers/peticion_mailer.rb
@@ -11,6 +11,6 @@ class PeticionMailer < ApplicationMailer
   def enviar_respuesta_idse
     peticion = params[:peticion]
     attachments["Respuesta_IDSE.pdf"] = { content: peticion.respuesta_idse.blob.download }
-    mail(to: peticion.usuario_externo.nombre_usuario, subject: "Documento IDSE. Peticion #" + peticion.folio.to_s + ". " + peticion.nombre_trabajador + " " + peticion.apellido_paterno + " " + peticion.apellido_materno)
+    mail(to: peticion.usuario_externo.nombre_usuario, subject: "Documento IDSE. Peticion #" + peticion.folio.to_s + ". " + peticion.nombre_trabajador + " " + peticion.apellido_paterno + " " + peticion.apellido_materno, reply_to: Rails.application.credentials.dig(:mail,:email))
   end
 end
